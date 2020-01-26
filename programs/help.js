59c15abc1ecf49e21a559f00d35e85ceca95b035
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-01-26 10:02:40 -0500

    update help file

--- programs/help.js
+++ programs/help.js
@@ -5,9 +5,13 @@ module.exports = function() {
         resolve(`
 open_mod <mod name> Opens a mod.
 save_mod Saves the currently opened mod.
-set_province_owner <province id> <tag>|none Sets the province owner to the provided country tag or removes ownership if no country tag is provided.
-set_province_core <province id> <layer> <tag>|none Sets the province core on the specified layer to the provided country tag or removes the core on the specified layer if no country tag is provided.
+set_province_owner <province id> <tag> Sets the province owner to the specified country or no country if no tag is specified.
+set_province_core <province id> <layer> <tag> Sets the province core on the specified layer to the specified country or no country if no tag is specified.
 get_province_data <province id> Gets the data for the province.
+create_province_owner_map <output path> Creates a map of province ownership at the specified path.
+create_province_core_map <layer> <output path> Creates a map of province cores on the specified layer at the specified path.
+set_province_owner_map <input path> Sets province ownership to the province ownership specified in the input path.
+set_province_core_map <layer> <input path> Sets province cores on the specified layer to the province cores specified in the input path.
 `)
     })
 }
--- programs/set_province_core.js
+++ programs/set_province_core.js
@@ -4,7 +4,7 @@ module.exports = function(commandLine, provinceId, coreIndex, tag) {
     return new Promise(resolve => {
         if (commandLine.environment.mod) {
             if (coreIndex && parseInt(coreIndex) && parseInt(provinceId) && commandLine.environment.mod.getProvince(parseInt(provinceId))) {
-                coreIndex = parseInt(coreIndex) + 1
+                coreIndex = parseInt(coreIndex) - 1
                 const province = commandLine.environment.mod.getProvince(parseInt(provinceId))
                 commandLine.environment.mod.setProvinceCore(province, coreIndex, tag)
                 resolve(`Province core on ${coreIndex} was set to ${tag ? tag : "no one"}`)
